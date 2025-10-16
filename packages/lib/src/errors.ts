d50d89d38643ae482cf4392b1e1194283588a733
Tom Scholz <tom.scholz@a-labs.io>
2025-10-16 14:39:00 +0200

    Modernize config to node 22 presets
    to also fix issues with not being able to target custom errors with
    instanceof

--- packages/lib/src/errors.ts
+++ packages/lib/src/errors.ts
@@ -12,7 +12,6 @@ function formatOperationName(request: RawRequest) {
 export abstract class ApiError extends Error {
   protected constructor(readonly request: RawRequest, message?: string) {
     super(message);
-    this.name = this.constructor.name;
   }
 }
 
