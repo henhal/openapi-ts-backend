5c368e1b494b361214dee58545dd92c8514c1578
Lucas Romero <luqasn@gmail.com>
2025-11-08 21:48:20 +0100

    Add back name override for custom error
    so that logging them shows the proper subtype

--- packages/lib/src/errors.ts
+++ packages/lib/src/errors.ts
@@ -12,6 +12,7 @@ function formatOperationName(request: RawRequest) {
 export abstract class ApiError extends Error {
   protected constructor(readonly request: RawRequest, message?: string) {
     super(message);
+    this.name = this.constructor.name;
   }
 }
 
