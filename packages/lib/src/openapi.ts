b33fc4998c283cf7d1e092aa8ca7d02da2d6822f
Lucas Romero <luqasn@gmail.com>
2025-12-05 10:37:03 +0000

    Allow disabling response validation (#13)
    Allow setting response validation strategy "none"

--- packages/lib/src/openapi.ts
+++ packages/lib/src/openapi.ts
@@ -35,7 +35,7 @@ import {
 } from './utils';
 import {createLogger, Logger, LogLevel} from './logger';
 
-type FailStrategy = 'warn' | 'throw';
+type ResponseValidationStrategy = 'warn' | 'throw' | 'none';
 type ResponseTrimming = 'none' | 'failing' | 'all';
 
 type HandlerData<P> = {
@@ -69,7 +69,7 @@ function isRawRequest(req: any): req is RawRequest {
 export interface Options<T> {
   errorHandler?: ErrorHandler<T>;
   logger?: Logger | null;
-  responseValidationStrategy?: FailStrategy;
+  responseValidationStrategy?: ResponseValidationStrategy;
   responseBodyTrimming?: ResponseTrimming;
   ajvOptions?: Ajv.Options;
 }
@@ -88,7 +88,7 @@ export class OpenApi<T> {
 
   readonly errorHandler: ErrorHandler<T>;
   readonly logger: Logger;
-  readonly responseValidationStrategy: FailStrategy;
+  readonly responseValidationStrategy: ResponseValidationStrategy;
   readonly responseBodyTrimming: ResponseTrimming;
   readonly ajvOptions?: Ajv.Options;
 
@@ -260,7 +260,9 @@ export class OpenApi<T> {
       // If status code is not specified and a non-ambiguous default status code is available, use it
       res.statusCode = res.statusCode ?? this.getDefaultStatusCode(operation);
 
-      this.validateResponse(apiContext, res);
+      if (this.responseValidationStrategy !== 'none') {
+          this.validateResponse(apiContext, res);
+      }
     };
   }
 
@@ -290,13 +292,13 @@ export class OpenApi<T> {
   protected handleValidationErrors(
       errors: Ajv.ErrorObject[] | null | undefined,
       title: string,
-      strategy: FailStrategy): void {
+      strategy: ResponseValidationStrategy): void {
     if (errors?.length) {
       this.fail(`${title}: ${formatArray(errors, formatValidationError)}`, strategy);
     }
   }
 
-  protected fail(message: string, strategy: FailStrategy): void {
+  protected fail(message: string, strategy: ResponseValidationStrategy): void {
     if (strategy === 'throw') {
       throw new Error(message);
     }
